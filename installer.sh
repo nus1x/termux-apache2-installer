de149ab661eb92c71a75bcaa4515a52ba2aef431
nus1x <ceritarommy@gmail.com>
2022-11-24 01:12:37 +0700

    Termux Apache2 Installer

---
+++ installer.sh
@@ -0,0 +1,15 @@
+apt update && apt upgrade -y
+
+apt install php -y && apt install php-apache -y && apt install apache2 -y
+
+cp httpd.conf /$PREFIX/etc/apache2/httpd.conf
+
+clear
+
+echo "===[ DONE ]==="
+echo "You can run apache2 using apachectl command."
+echo "=============="
+echo "Don't forget to visit jurnalrommy.com"
+echo "=============="
+
+rm -rf ../termux-apache2-installer
