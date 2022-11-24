ffd47e5988c36a04fe0a69fc001a545609da6f65
nus1x <ceritarommy@gmail.com>
2022-11-24 07:13:19 +0700

    Termux Apache2 Installer

--- installer.sh
+++ installer.sh
@@ -2,12 +2,15 @@ apt update && apt upgrade -y
 
 apt install php -y && apt install php-apache -y && apt install apache2 -y
 
+mkdir /sdcard/htdocs
+
 cp httpd.conf /$PREFIX/etc/apache2/httpd.conf
 
 clear
 
 echo "===[ DONE ]==="
 echo "You can run apache2 using apachectl command."
+echo "local folder: /sdcard/htdocs"
 echo "=============="
 echo "Don't forget to visit jurnalrommy.com"
 echo "=============="
